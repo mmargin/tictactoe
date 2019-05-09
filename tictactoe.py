b1e88225bc4bb84f9a1ac92e0ecc6c2beb2b12ec
mmargin <mmargin@umich.edu>
2019-05-08 23:04:32 -0400

    set up complete

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 
-def intro(mode, xo):
-	print("Tic Tac Toe Game")
+def intro():
+	print("***** Tic Tac Toe Game *****")
 	print("This game can be played in 3 ways:")
 	print("1) Person vs. Computer")
 	print("2) Person vs. Person")
@@ -8,9 +8,10 @@ def intro(mode, xo):
 
 	start = 1
 	while start == 1:
-		mode = raw_input("Select a mode by indicating one number 1-3 ")
-		if mode == '1':
-			xo = raw_input("Would you, the Person, like to be 'x' or 'o'? " )
+		var[0] = raw_input("Select a mode by indicating one number 1-3 ")
+		if var[0] == '1':
+			var[1] = raw_input("Would you, the Person, like to be 'x' or 'o'? " )
+			xo = var[1]
 			if xo == 'x':
 				print("That means that the Computer will be 'o'")
 				start = 0
@@ -19,8 +20,9 @@ def intro(mode, xo):
 				start = 0
 			else:
 				print("Invalid input")
-		elif mode == '2':
-			xo = raw_input("Would you, Person1, like to be x or o? ")
+		elif var[0] == '2':
+			var[1] = raw_input("Would you, Person1, like to be x or o? ")
+			xo = var[1]
 			if xo == 'x':
 				print("That means that Person2 will be 'o'")
 				start = 0
@@ -29,28 +31,38 @@ def intro(mode, xo):
 				start = 0
 			else:
 				print("Invalid input")
-		elif mode == '3':
+		elif var[0] == '3':
 			print("Computer1 will be 'x' and Computer2 will be 'o' ")
 			start = 0
 		else:
-			print("Invalid input")
-	
+			print("Invalid input")	
 	raw_input("Press ENTER to continue ")
 
-def game(mode):
-	print(mode)
-def user1(xo):
-	print(xo)
+def mode():
+	return var[0] # MODE = var[0]
 
-player1 = ['','','','','','','','',''] # board in list form 0-8
-player2 = ['','','','','','','','',''] # board in list form 0-8
+def user1():
+	return var[1] # Player1 choice = var[1]
 
-mode = '0'
-xo = 'x'
-intro(mode, xo)
-game(mode)
-user1(xo)
+def printBoard():
+	print(board[0] + '|' + board[1] + '|' + board[2])
+	print('-----')
+	print(board[3] + '|' + board[4] + '|' + board[5])
+	print('-----')
+	print(board[6] + '|' + board[7] + '|' + board[8])
+
+def game():
+	mode = var[0]
+	player1 = var[1]
+	if mode == 1: # ask player 1 for the moves and have computer respond with other char
+	if mode == 2: # ask player 1 and then ask player 2 VALIDATE
+	if mode == 3: # human watches comp play itself
 
 
+# MAIN
+board = ['','','','','','','','',''] # board in list form 0-8
 
-
+var = ['0','0']
+intro()
+mode()
+user1()
